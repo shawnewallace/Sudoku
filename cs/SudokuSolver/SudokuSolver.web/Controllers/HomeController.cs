0e13928b12c1c25483f1d92e0279f9ab8fa2334a
Shawn Wallace <shawn@the-wallaces.net>
2013-11-15 14:48:17 -0500

    Updated web site and added Azure deployment target

--- cs/SudokuSolver/SudokuSolver.web/Controllers/HomeController.cs
+++ cs/SudokuSolver/SudokuSolver.web/Controllers/HomeController.cs
@@ -1,44 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using SudokuSolver.lib;
 using SudokuSolver.web.Models;
 
 namespace SudokuSolver.web.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly SudokuSolverCs _service;
+  public class HomeController : Controller
+  {
+    private readonly SudokuSolverCs _service;
+    private List<string> _solvedModels;
 
-		public HomeController()
-		{
-			_service = new SudokuSolverCs();
-		}
+    public HomeController()
+    {
+      _service = new SudokuSolverCs();
+      _solvedModels = new List<string>
+            {
+                "85...24..72......9..4.........1.7..23.5...9...4...........8..7..17..........36.4.",
+                "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......",
+                "52...6.........7.13...........4..8..6......5...........418.........3..2...87....."
+            };
+    }
 
-		public ActionResult Index()
-		{
-			var model = new BoardUIModel(_service.get_board(null));
+    public ActionResult Index()
+    {
+      var model = new BoardUIModel(_service.get_board(null)) { OriginalInput = _solvedModels };
 
-			return View(model);
-		}
+      return View(model);
+    }
 
-		[HttpPost]
-		public ActionResult Board(string input)
-		{
-			var model = new BoardUIModel(_service.get_board(input));
+    [HttpPost]
+    public ActionResult Board(string input)
+    {
+      AddInputToSolvedModels(input);
+      var model = new BoardUIModel(_service.get_board(input)) { OriginalInput = _solvedModels };
+      return View("Index", model);
+    }
 
-			return View("Index", model);
-		}
+    [HttpPost]
+    public ActionResult Solve(string[] values)
+    {
+      values = CleanValues(values);
+      var input = string.Join("", values);
+      AddInputToSolvedModels(input);
+      var model = new BoardUIModel(_service.search(_service.parse_grid(input))) { OriginalInput = _solvedModels };
 
-		[HttpPost]
-		public ActionResult Solve(string[] values)
-		{
-			var input = string.Join("", values);
-			var model = new BoardUIModel(_service.search(_service.parse_grid(input)));
+      return View("Index", model);
+    }
 
-			return View("Index", model);
-		}
-	}
+    private string[] CleanValues(string[] values)
+    {
+      for (var i = 0; i < values.Length; i++)
+      {
+        if (values[i].Trim() == "") values[i] = ".";
+      }
+      return values;
+    }
+
+    private void AddInputToSolvedModels(string input)
+    {
+      if (_solvedModels.Contains(input)) return;
+
+      _solvedModels.Add(input);
+    }
+  }
 }
--- cs/SudokuSolver/SudokuSolver.web/Models/BoardUIModel.cs
+++ cs/SudokuSolver/SudokuSolver.web/Models/BoardUIModel.cs
@@ -6,40 +6,43 @@ using SudokuSolver.lib;
 
 namespace SudokuSolver.web.Models
 {
-	public class BoardUIModel : Dictionary<string, string>
-	{
-		public BoardUIModel()
-		{
-		}
+    public class BoardUIModel : Dictionary<string, string>
+    {
+        public List<string> OriginalInput { get; set; }
 
-		public BoardUIModel(string grid)
-		{
-		}
-		
-		public BoardUIModel(Dictionary<string, string> board)
-		{
-			foreach (var cell in board)
-			{
-				Add(cell.Key, cell.Value);
-			}
-		}
+        public BoardUIModel()
+        {
+        }
 
-		public string WhichUnit(string cell)
-		{
-			var row = cell[0];
-			var col = cell[1];
+        public BoardUIModel(string grid)
+        {
+        }
 
-			if ("ABC".Contains(row) && "123".Contains(col)) return "A";
-			if ("DEF".Contains(row) && "123".Contains(col)) return "B";
-			if ("GHI".Contains(row) && "123".Contains(col)) return "C";
-			if ("ABC".Contains(row) && "456".Contains(col)) return "D";
-			if ("DEF".Contains(row) && "456".Contains(col)) return "E";
-			if ("GHI".Contains(row) && "456".Contains(col)) return "F";
-			if ("ABC".Contains(row) && "789".Contains(col)) return "G";
-			if ("DEF".Contains(row) && "789".Contains(col)) return "H";
-			if ("GHI".Contains(row) && "789".Contains(col)) return "I";
+        public BoardUIModel(Dictionary<string, string> board)
+        {
+            foreach (var cell in board)
+            {
+                Add(cell.Key, cell.Value);
+            }
+        }
 
-			return "";
-		}
-	}
+        public string WhichUnit(string cell)
+        {
+            var row = cell[0];
+            var col = cell[1];
+
+            if ("ABC".Contains(row) && "123".Contains(col)) return "A";
+            if ("DEF".Contains(row) && "123".Contains(col)) return "B";
+            if ("GHI".Contains(row) && "123".Contains(col)) return "C";
+            if ("ABC".Contains(row) && "456".Contains(col)) return "D";
+            if ("DEF".Contains(row) && "456".Contains(col)) return "E";
+            if ("GHI".Contains(row) && "456".Contains(col)) return "F";
+            if ("ABC".Contains(row) && "789".Contains(col)) return "G";
+            if ("DEF".Contains(row) && "789".Contains(col)) return "H";
+            if ("GHI".Contains(row) && "789".Contains(col)) return "I";
+
+            return "";
+        }
+
+    }
 }
